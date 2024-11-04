bf7e6420f5c447dab24be11eef4260aa72249dee
Fastpitch Studio <dalena@fastpitchstudio.com>
2024-11-04 02:18:13 -0800

    refactor: address TypeScript typing issue in TeamPage component

--- src/app/dashboard/teams/[id]/page.tsx
+++ src/app/dashboard/teams/[id]/page.tsx
@@ -1,10 +1,7 @@
 // src/app/dashboard/teams/[id]/page.tsx
 import TeamPageContent from './team-page-content'
 
-export default function TeamPage(props: any) {
+// @ts-expect-error - Next.js page props typing issue
+export default function TeamPage(props) {
   return <TeamPageContent teamId={props.params.id} />
 }
-
-// We're disabling the ESLint rule for this specific case
-// since we need to use 'any' to work with Next.js internal types
-/* eslint-disable @typescript-eslint/no-explicit-any */
