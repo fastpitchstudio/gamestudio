cb04362e3baee8c489a66949f652b698137c9988
Fastpitch Studio <dalena@fastpitchstudio.com>
2024-11-21 23:37:29 -0800

    fix for name camelcase issues

--- src/components/game/lineup-v2/utils.ts
+++ src/components/game/lineup-v2/utils.ts
@@ -1,7 +1,7 @@
 import { Player } from '@/types/player';
 
 export const getPlayerInitials = (player: Player): string => {
-  const firstInitial = player.first_name?.[0] || '';
-  const lastInitial = player.last_name?.[0] || '';
+  const firstInitial = player.firstName?.[0] || '';
+  const lastInitial = player.lastName?.[0] || '';
   return `${firstInitial}${lastInitial}`.toUpperCase();
 };
